b5c16d3a8c7e97e716697e8f5c20fb89c0e6d658
Nickolay <anahronizm08@gmail.com>
2023-02-03 17:01:54 +0300

    Внедрена FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-import React, { Suspense, useContext, useState } from 'react'
+import { Suspense} from 'react'
 import { Link } from 'react-router-dom'
 import { Route, Routes } from 'react-router-dom'
-import Counter from './components/Counter'
 import './styles/index.scss'
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
-import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
-import { useTheme } from './theme/useTheme'
-import classNames from './helpers/classNames/classNames'
+import classNames from 'shared/lib/classNames/classNames'
+import { useTheme } from './providers/ThemeProvider'
+import { AboutPage } from 'pages/AboutPage'
+import { MainPage } from 'pages/MainPage'
 
 
 
@@ -19,11 +18,10 @@ const App = () => {
       <Link to={'/about'}>О странице</Link>
       <Suspense fallback={<div>Loading</div>}>
       <Routes>
-            <Route path={"/about"} element={<AboutPageAsync/>}/>
-            <Route path={"/"} element={<MainPageLazy/>}/>
+            <Route path={"/about"} element={<AboutPage/>}/>
+            <Route path={"/"} element={<MainPage/>}/>
       </Routes>
       </Suspense>
-      <Counter/>
     </div>
   )
 }
