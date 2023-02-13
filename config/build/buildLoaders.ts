8ac153fc0f37bc666888e19d2ff2a93474619545
Nickolay <anahronizm08@gmail.com>
2023-02-13 11:01:22 +0300

    Добавлен Jest для тестирования

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,7 +3,21 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BuildOptions } from './types/config';
 
 export function buildLoaders(options:BuildOptions):webpack.RuleSetRule[]{
-    const typescript_loader = {
+  
+  const babelLoader = {
+      // test: /\.m?js$/,
+      test: /\.(js|jsx|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env']
+            }
+    }
+  }
+  
+  
+  const typescript_loader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
@@ -30,6 +44,7 @@ export function buildLoaders(options:BuildOptions):webpack.RuleSetRule[]{
         ]
     }
     return [
+        babelLoader,
         typescript_loader,
         scss_loader
       ]
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,5 @@
+describe("classnames",()=>{
+    test("firsttest",()=>{
+        expect(1).toBe(1)
+    })
+})
