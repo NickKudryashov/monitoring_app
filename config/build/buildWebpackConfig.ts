44bf7c35e06299589973fc4b1a90ed7d76b653ee
Nickolay <anahronizm08@gmail.com>
2023-05-25 17:39:22 +0300

    fixed config

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -23,7 +23,5 @@ export function buildWebpackConfig(options:BuildOptions):webpack.Configuration {
         resolve: buildResolvers(options),
         plugins: buildPLugins(options),
         devtool:isDev ? "inline-source-map" : undefined,
-        devServer:isDev ? buildDevServer(options) : undefined,
-        target:"node"
-    };
+        devServer:isDev ? buildDevServer(options) : undefined    };
 }
