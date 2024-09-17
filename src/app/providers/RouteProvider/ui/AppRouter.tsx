3daf9f169f4a971aeb158eced21c71a5e22c5c14
NickKudryashov <wemakesomemagic@gmail.com>
2024-09-17 18:11:19 +0300

    *

--- src/app/providers/RouteProvider/ui/AppRouter.tsx
+++ src/app/providers/RouteProvider/ui/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { defaultAuthCheck, getIsAuth, getUserData } from "@/entities/user";
 import { Suspense, useEffect, useLayoutEffect } from "react";
 import { useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useAppDispatch } from "@/shared/hooks/hooks";
 import { RouteConfigPublic, RouteConfigAuth } from "../config/RouteConfig";
 import { MainLayoutPageLoader } from "@/pages/MainLayoutPageLoader";
@@ -9,15 +9,21 @@ import { MainLayout } from "@/shared/ui/MainLayout/MainLayout";
 import { Navbar } from "@/widgets/Navbar";
 import { Sidebar } from "@/widgets/Sidebar";
 import { getVersion } from "@/entities/user/Store/actionCreators";
+import { useMobilDeviceDetect } from "@/shared/hooks/useMobileDeviceDetect";
+import { RoutePathPublic } from "@/shared/config/RouteConfig/RouteConfig";
 
 export const AppRouter = () => {
     const isAuth = useSelector(getIsAuth);
     const dispatch = useAppDispatch();
-
+    const isMobile = useMobilDeviceDetect();
+    const navigate = useNavigate();
     useEffect(() => {
         dispatch(defaultAuthCheck());
         dispatch(getUserData());
         dispatch(getVersion());
+        if (!isAuth && isMobile) {
+            navigate(RoutePathPublic.auth);
+        }
     }, [dispatch]);
 
     return (
--- src/pages/MockPage/ui/MockPage.tsx
+++ src/pages/MockPage/ui/MockPage.tsx
@@ -1,15 +1,19 @@
 import classNames from "@/shared/lib/classNames/classNames";
-import { memo } from "react";
+import { memo, useLayoutEffect } from "react";
 import cls from "./MockPage.module.scss";
 import type { PropsWithChildren } from "react";
 import { Navbar } from "@/widgets/Navbar";
 import { VFlexBox } from "@/shared/ui/FlexBox/VFlexBox/VFlexBox";
+import { useMobilDeviceDetect } from "@/shared/hooks/useMobileDeviceDetect";
+import { useNavigate } from "react-router-dom";
+import { RoutePathPublic } from "@/shared/config/RouteConfig/RouteConfig";
 interface MockPageProps {
     className?: string;
 }
 
 const MockPage = memo((props: PropsWithChildren<MockPageProps>) => {
     const { className = "" } = props;
+
     return (
         <div
             data-testid="MockPage"
